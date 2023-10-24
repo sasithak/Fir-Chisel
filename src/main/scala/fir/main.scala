534f777f445174851929979e7f3031e5badfb875
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2023-10-24 12:06:26 +0530

    Bitwidth

--- src/main/scala/fir/main.scala
+++ src/main/scala/fir/main.scala
@@ -5,7 +5,8 @@ import chisel3.util._
 import chisel3.stage.{ChiselStage, ChiselGeneratorAnnotation}
 
 object MainApp extends App {
-  val coefficients = Utils.readCoefficients(8)
-  val fir_verilog = Utils.emitVerilog(new FirFilter(8, coefficients))
+  val bitWidth = 16
+  val coefficients = Utils.readCoefficients(bitWidth)
+  val fir_verilog = Utils.emitVerilog(new FirFilter(bitWidth, coefficients))
   println(fir_verilog)
 }
